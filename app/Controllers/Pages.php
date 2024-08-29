ea350ca0a2826ae366301d59802a1d15580fd5d3
mtraYuriCoronado <mtra.yuricoronado@gmail.com>
2024-08-29 13:00:21 -0600

    agregaron vistas y controladores

--- app/Controllers/Pages.php
+++ app/Controllers/Pages.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Controllers;
+use CodeIgniter\Exceptions\PageNotFoundException;
 
 class Pages extends BaseController
 {
@@ -11,6 +12,15 @@ class Pages extends BaseController
 
     public function view(string $page = 'home')
     {
-        // ...
+        if (! is_file(APPPATH . 'Views/pages/' . $page . '.php')) {
+            // Whoops, we don't have a page for that!
+            throw new PageNotFoundException($page);
+        }
+
+        $data['title'] = ucfirst($page); // Capitalize the first letter
+
+        return view('templates/header', $data)
+            . view('pages/' . $page)
+            . view('templates/footer');
     }
 }
