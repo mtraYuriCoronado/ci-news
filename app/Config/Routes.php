f8653565dd31ed1736474a4c969e22f880560bba
mtraYuriCoronado <mtra.yuricoronado@gmail.com>
2024-08-29 13:51:08 -0600

    se crean las rutas para las noticias usando el controlador news

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -1,11 +1,18 @@
 <?php
 
 use CodeIgniter\Router\RouteCollection;
+
+use App\Controllers\News; // Add this line
 use App\Controllers\Pages;
 
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
+// $routes->get('/', 'Home::index');
+
+
+$routes->get('news', [News::class, 'index']);           // Add this line
+$routes->get('news/(:segment)', [News::class, 'show']); // Add this line
+
 $routes->get('pages', [Pages::class, 'index']);
 $routes->get('(:segment)', [Pages::class, 'view']);
