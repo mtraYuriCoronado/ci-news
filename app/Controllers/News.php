499c5d3ad9149f40fb7b3056d288b9413e6c5dde
mtraYuriCoronado <mtra.yuricoronado@gmail.com>
2024-08-29 14:03:47 -0600

    ver noticias individuales: se modifico el controlador y se creó la vista para las noticias individuales

--- app/Controllers/News.php
+++ app/Controllers/News.php
@@ -3,6 +3,7 @@
 namespace App\Controllers;
 
 use App\Models\NewsModel;
+use CodeIgniter\Exceptions\PageNotFoundException;
 
 class News extends BaseController
 {
@@ -25,5 +26,15 @@ class News extends BaseController
         $model = model(NewsModel::class);
 
         $data['news'] = $model->getNews($slug);
+
+        if ($data['news'] === null) {
+            throw new PageNotFoundException('Cannot find the news item: ' . $slug);
+        }
+
+        $data['title'] = $data['news']['title'];
+
+        return view('templates/header', $data)
+            . view('news/view')
+            . view('templates/footer');
     }
 }
